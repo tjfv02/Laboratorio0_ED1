163bba35c640339a9117d66360a59f4edbffc4ad
tjfv02 <tjfv02@gmail.com>
2020-04-10 00:42:43 -0600

    Ingreso de Usuarios

---
+++ Lab0_Estructuras/Controllers/ClientesController.cs
@@ -0,0 +1,120 @@
+﻿using Lab0_Estructuras.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab0_Estructuras.Controllers
+{
+    
+    public class ClientesController : Controller
+    {
+        public static List<Cliente> Guardando = new List<Cliente>();
+
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            return View(Guardando);
+        }
+
+        // GET: Clientes/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Clientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clientes/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                Cliente Ingreso = new Cliente()
+                {
+
+                    Nombre=collection["Nombre"],
+                    Apellido=collection["Apellido"],
+                    Telefono=collection["Telefono"],
+                    Descripcion=collection["Descripcion"]
+
+                };
+                Guardando.Add(Ingreso);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Clientes/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                Cliente Eliminado = new Cliente()
+                {
+                    Nombre = collection["Nombre"],
+                    Apellido = collection["Apellido"],
+                    Telefono = collection["Telefono"],
+                    Descripcion = collection["Descripcion"]
+
+                };
+                Guardando.Remove(Eliminado);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+
+            }
+        }
+        public ActionResult OrdenarNombre()
+        {
+            Guardando.Sort((x, y) => x.Nombre.CompareTo(y.Nombre));
+            return View(Guardando);
+        }
+        public ActionResult OrdenarApellido()
+        {
+            Guardando.Sort((x, y) => x.Apellido.CompareTo(y.Apellido));
+            return View(Guardando);
+        }
+    }
+}
---
+++ Lab0_Estructuras/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab0_Estructuras.Models
+{
+    public class Cliente
+    {
+        public int Id {get; set;}
+        public string Nombre    {get; set;}
+        public string Apellido  {get; set;}
+        public string Telefono  {get; set;}
+        public string Descripcion{get; set;}
+    }
+}
